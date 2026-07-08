30098a7f9bf598ebaf220647fab022e73b1ba9a5
Karim Zerman <whiskaswannacry@gmail.com>
2026-07-08 19:46:31 +0300

    feat-be: add forbidNonWhitelisted to ValidationPipe

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -69,6 +69,7 @@ async function bootstrap() {
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
+      forbidNonWhitelisted: true,
       transform: true,
       stopAtFirstError: true,
     }),
